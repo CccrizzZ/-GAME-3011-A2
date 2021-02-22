b66ad928e6e2153c59a2000542681c11f6425a19
cccrizzz <ccccrizzzz@gmail.com>
2021-02-23 04:52:35 +0800

    Added picking mechanics

--- Assets/Scripts/LockPickingCanvasScript.cs
+++ Assets/Scripts/LockPickingCanvasScript.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,42 +28,81 @@ public class LockPickingCanvasScript : MonoBehaviour
         Unlocked = false;
 
 
+        // get pin level
         PinsInitLevel = PinsRefArray[0].transform.position.y;
+
+
+        // randomize unlock level for each pin
+        foreach (var pin in PinsRefArray)
+        {
+            pin.transform.GetChild(0).transform.position = new Vector3(
+                pin.transform.GetChild(0).transform.position.x,
+                pin.transform.GetChild(0).transform.position.y + Random.Range(0.24f,-0.24f),
+                pin.transform.GetChild(0).transform.position.z
+            );
+        }
+        
+
+
+
     }
 
 
     void Update()
     {
-        if(Input.GetKeyDown("d"))
+        // If key is on the 5th pin lock is picked
+        if (ToolPosition == 5)
         {
-            ShiftPickingTool(1);
+            Unlocked = true;
+        }else
+        {
+            Unlocked = false;
         }
 
 
+        // push the key inwards
+        if(Input.GetKeyDown("d") && !Unlocked)
+        {
+            if (PinsRefArray[ToolPosition-1].transform.GetChild(0).transform.position.y <= TargetLineRef.transform.position.y + 0.01f 
+            && PinsRefArray[ToolPosition-1].transform.GetChild(0).transform.position.y >= TargetLineRef.transform.position.y - 0.01f )
+            {
+                ShiftPickingTool(1);
+                
+            }
+        }
 
+
+        // pull the key outwards
         if(Input.GetKeyDown("a"))
         {
             ShiftPickingTool(-1);
         }
 
-        if (Input.GetKeyDown("w"))
-        {
-            print("111");
-            up = true;
+        if (!Unlocked)
+        { 
+            // push the pin up
+            if (Input.GetKeyDown("w"))
+            {
+                print("111");
+                up = true;
+            }
 
+
+            // let the pin down
+            if (Input.GetKeyUp("w"))
+            {
+                print("222");
+                up = false;
+            }
+                
         }
 
 
-        if (Input.GetKeyUp("w"))
-        {
-            print("222");
-            up = false;
-        
-        }
 
         // w key pressed
         if (up)
         {
+            // set pin transform
             if(PinsRefArray[ToolPosition-1].transform.position.y <= PinsInitLevel + 1f)
             {
                 PinsRefArray[ToolPosition-1].transform.position = new Vector3(
@@ -72,6 +112,7 @@ public class LockPickingCanvasScript : MonoBehaviour
                 );
             }
 
+            // set spring scale
             if (SpringRefArray[ToolPosition-1].transform.localScale.y > 0.25f)
             {
                 SpringRefArray[ToolPosition-1].transform.localScale = new Vector3(
@@ -85,8 +126,9 @@ public class LockPickingCanvasScript : MonoBehaviour
 
 
         // w key released
-        if(!up)
+        if(!up && ToolPosition <= 4)
         {
+            // set pin transform
             if(PinsRefArray[ToolPosition-1].transform.position.y >= PinsInitLevel + 0.18f)
             {
                 PinsRefArray[ToolPosition-1].transform.position = new Vector3(
@@ -97,7 +139,7 @@ public class LockPickingCanvasScript : MonoBehaviour
 
             }
 
-
+            // set spring scale
             if (SpringRefArray[ToolPosition-1].transform.localScale.y < 1f)
             {
                 SpringRefArray[ToolPosition-1].transform.localScale = new Vector3(
@@ -117,7 +159,7 @@ public class LockPickingCanvasScript : MonoBehaviour
     void ShiftPickingTool(int diff)
     {
         // go next
-        if (ToolPosition + 1 <= 4 && diff == 1)
+        if (ToolPosition + 1 <= 5 && diff == 1)
         {
             // set tool position
             ToolPosition += diff;
