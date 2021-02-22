a9511168fe444eb16a32f29d1a3972bbcfcc9e19
cccrizzz <ccccrizzzz@gmail.com>
2021-02-23 04:06:31 +0800

    Added spring and pin movement

--- Assets/Scripts/LockPickingCanvasScript.cs
+++ Assets/Scripts/LockPickingCanvasScript.cs
@@ -7,12 +7,17 @@ public class LockPickingCanvasScript : MonoBehaviour
 
     public GameObject ToolRef;
     public GameObject TargetLineRef;
+    public GameObject[] PinsRefArray;
+    public GameObject[] SpringRefArray;
+
 
 
     int ToolPosition;
     bool Unlocked;
+    
+    bool up;
 
-
+    float PinsInitLevel;
 
 
 
@@ -20,6 +25,9 @@ public class LockPickingCanvasScript : MonoBehaviour
     {
         ToolPosition = 1;
         Unlocked = false;
+
+
+        PinsInitLevel = PinsRefArray[0].transform.position.y;
     }
 
 
@@ -36,6 +44,71 @@ public class LockPickingCanvasScript : MonoBehaviour
         {
             ShiftPickingTool(-1);
         }
+
+        if (Input.GetKeyDown("w"))
+        {
+            print("111");
+            up = true;
+
+        }
+
+
+        if (Input.GetKeyUp("w"))
+        {
+            print("222");
+            up = false;
+        
+        }
+
+        // w key pressed
+        if (up)
+        {
+            if(PinsRefArray[ToolPosition-1].transform.position.y <= PinsInitLevel + 1f)
+            {
+                PinsRefArray[ToolPosition-1].transform.position = new Vector3(
+                    PinsRefArray[ToolPosition-1].transform.position.x,
+                    PinsRefArray[ToolPosition-1].transform.position.y + 0.005f,
+                    PinsRefArray[ToolPosition-1].transform.position.z
+                );
+            }
+
+            if (SpringRefArray[ToolPosition-1].transform.localScale.y > 0.25f)
+            {
+                SpringRefArray[ToolPosition-1].transform.localScale = new Vector3(
+                    SpringRefArray[ToolPosition-1].transform.localScale.x,
+                    SpringRefArray[ToolPosition-1].transform.localScale.y - 0.0048f,
+                    SpringRefArray[ToolPosition-1].transform.localScale.z
+                );
+            }
+        }
+
+
+
+        // w key released
+        if(!up)
+        {
+            if(PinsRefArray[ToolPosition-1].transform.position.y >= PinsInitLevel + 0.18f)
+            {
+                PinsRefArray[ToolPosition-1].transform.position = new Vector3(
+                    PinsRefArray[ToolPosition-1].transform.position.x,
+                    PinsRefArray[ToolPosition-1].transform.position.y - 0.005f,
+                    PinsRefArray[ToolPosition-1].transform.position.z
+                );
+
+            }
+
+
+            if (SpringRefArray[ToolPosition-1].transform.localScale.y < 1f)
+            {
+                SpringRefArray[ToolPosition-1].transform.localScale = new Vector3(
+                    SpringRefArray[ToolPosition-1].transform.localScale.x,
+                    SpringRefArray[ToolPosition-1].transform.localScale.y + 0.0048f,
+                    SpringRefArray[ToolPosition-1].transform.localScale.z
+                );
+            }
+        }
+
+
     }
 
 
@@ -43,15 +116,37 @@ public class LockPickingCanvasScript : MonoBehaviour
     // shift the tool back and forth
     void ShiftPickingTool(int diff)
     {
-        // set tool position
-        ToolPosition += diff;
+        // go next
+        if (ToolPosition + 1 <= 4 && diff == 1)
+        {
+            // set tool position
+            ToolPosition += diff;
 
-        // set tool transform
-        ToolRef.transform.position = new Vector3(
-            ToolRef.transform.position.x + 0.4f * diff,
-            ToolRef.transform.position.y,
-            ToolRef.transform.position.z
-        );
+            // set tool transform
+            ToolRef.transform.position = new Vector3(
+                ToolRef.transform.position.x + 0.8f,
+                ToolRef.transform.position.y,
+                ToolRef.transform.position.z
+            );
+        }
+        
+
+        // go previous
+        if(ToolPosition - 1 >= 1 && diff == -1)
+        {
+            // set tool position
+            ToolPosition += diff;
+
+            // set tool transform
+            ToolRef.transform.position = new Vector3(
+                ToolRef.transform.position.x - 0.8f,
+                ToolRef.transform.position.y,
+                ToolRef.transform.position.z
+            );
+        }
+
+        // print(ToolPosition);
+
     }
 
 
